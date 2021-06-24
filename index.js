39cb0efa9fa93d92dc6a71585d361127971fa9f2
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-06-24 21:27:10 +0600

    third commit

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
+
 const app = express()
 const port = process.env.PORT || 5000
 
-
+app.use(cors());
+app.use(bodyParser.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.clvrh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 console.log(uri)
@@ -14,7 +17,38 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const blogsCollection = client.db("retroBlog").collection("blogs");
   console.log('database connected')
-  // perform actions on the collection object
+
+  app.post('/addBlog', (req, res) => {
+    const newBlog = req.body;
+    blogsCollection.insertOne(newBlog)
+      .then(result => {
+        console.log('inserted', result.insertedCount)
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.get('/blogs', (req, res) => {
+    blogsCollection.find()
+      .toArray((err, events) => {
+        res.send(events)
+      })
+  })
+
+  app.get('/blog/:id', (req, res) => {
+    blogsCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0])
+      })
+  })
+
+
+
+
+
+
+
+
+
 });
 
 
